8c898ee59c489fba30d65e3c338f5789875f37af
wallase <wallase.huang@gmail.com>
2025-07-04 06:34:09 +0800

    部署 cros 問題

--- apps/backend/next.config.js
+++ apps/backend/next.config.js
@@ -16,7 +16,10 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'http://localhost:3000' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.PUBLIC_FRONTEND_URL || '*',
+          },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
